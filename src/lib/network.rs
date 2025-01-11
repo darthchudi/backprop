8cc22c3d6516b463634870aa5a2ed0493a86a041
darthchudi <chukwudioranu@ymail.com>
2025-01-11 21:11:18 +0000

    update test

--- src/lib/network.rs
+++ src/lib/network.rs
@@ -11,103 +11,127 @@ pub struct Neuron {
     bias: Bias,
 }
 
-// // Neuron represents a single neuron with a given weight and bias value
-// impl Neuron {
-//     fn new(inputs: u64) -> Neuron {
-//         let mut weights_rng = rand::thread_rng();
-//         let mut bias_rng = rand::thread_rng();
-//
-//         let mut weights: Vec<Value<f64>> = Vec::with_capacity(inputs as usize);
-//         for _ in 0..inputs {
-//             let raw_weight = weights_rng.gen_range(-1.0..=1.0);
-//            let weight = Value::new(raw_weight);
-//
-//             weights.push(weight);
-//         }
-//
-//         let raw_bias = bias_rng.gen_range(-0.01..=0.01);
-//         let bias = Value::new(raw_bias);
-//
-//         Neuron {
-//             weights,
-//             bias
-//         }
-//     }
-//
-//     fn relu(x: f64) -> f64 {
-//         x.max(0.0)
-//     }
-//
-//     // Performs the forward pass on a given input and returns the activation
-//     fn forward(&self, x: &Vec<f64>) -> f64 {
-//         let mut weight_sum = Value::new(0.0);
-//
-//         if x.len() != self.weights.len() {
-//             panic!("{}", format!("{} input dimensions not compatible with {} weight dimensions", x.len(), self.weights.len()));
-//         }
-//
-//         // Compute the weighted sum of inputs for the neuron.
-//         for (index, input_val) in x.iter().enumerate() {
-//             let computed_val = self.weights[index]  * Value::new_from_ref(input_val);  // Wi * Xi
-//             weight_sum = weight_sum + computed_val; // Sum(WnXn)
-//         }
-//
-//         let weight_and_bias = weight_sum + self.bias;
-//
-//         let activation = Neuron::relu(weight_and_bias.val());
-//
-//         activation
-//     }
-// }
-//
-// // Layer consists of a set of neurons which receive inputs
-// pub struct Layer {
-//     neurons: Vec<Neuron>
-// }
-//
-// impl Layer {
-//     pub fn new(num_inputs: u64, num_outputs: u64) -> Layer{
-//         let mut neurons = Vec::with_capacity(num_outputs as usize);
-//
-//         for _ in 0..num_outputs {
-//             let neuron = Neuron::new(num_inputs);
-//             neurons.push(neuron);
-//         }
-//
-//         Layer{neurons}
-//     }
-//
-//     fn forward(&self, inputs: &Vec<f64>) -> Vec<f64> {
-//         let mut outputs = Vec::with_capacity(self.neurons.len());
-//
-//         for neuron in &self.neurons{
-//             let neuron_result = neuron.forward(&inputs);
-//             outputs.push(neuron_result);
-//         }
-//
-//         outputs
-//     }
-// }
-//
-// pub struct Network {
-//    pub layers: Vec<Layer>
-// }
-//
-// impl Network {
-//     pub fn forward(&self, inputs: &Vec<f64>) -> Vec<f64> {
-//         let mut result = Vec::new();
-//
-//         for (index, layer) in self.layers.iter().enumerate(){
-//             if index == 0 {
-//                 // The first layer receives the inputs directly
-//                 result = layer.forward(&inputs);
-//                 continue
-//             }
-//
-//             // Subsequent layers will receive the previous layers output
-//             result = layer.forward(&result)
-//         }
-//
-//         result
-//     }
-// }
+// Neuron represents a single neuron with a given weight and bias value
+impl Neuron {
+    fn new(inputs: u64) -> Neuron {
+        let mut weights_rng = rand::thread_rng();
+        let mut bias_rng = rand::thread_rng();
+
+        let mut weights: Vec<Value<f64>> = Vec::with_capacity(inputs as usize);
+        for _ in 0..inputs {
+            let raw_weight = weights_rng.gen_range(-1.0..=1.0);
+           let weight = Value::new(raw_weight);
+
+            weights.push(weight);
+        }
+
+        let raw_bias = bias_rng.gen_range(-0.01..=0.01);
+        let bias = Value::new(raw_bias);
+
+        Neuron {
+            weights,
+            bias
+        }
+    }
+
+    fn relu(x: f64) -> f64 {
+        x.max(0.0)
+    }
+
+    // Performs the forward pass on a given input and returns the activation
+    fn forward(&self, x: &Vec<f64>) -> f64 {
+        // Compute the weighted sum of inputs for the neuron.
+        let weighted_sum = self.weights.
+            iter().
+            zip(x).
+            map(|(w, input)| {
+                let input_dim = &Value::new_from_ref(input);
+                
+                w * input_dim
+            }).
+            fold(Value::new(0.0), |acc, item| {
+                acc + item
+            });
+        
+        let weight_and_bias = &weighted_sum + &self.bias;
+
+        let activation = Neuron::relu(weight_and_bias.get_data());
+
+         activation
+    }
+}
+
+// Layer consists of a set of neurons which receive inputs
+pub struct Layer {
+    neurons: Vec<Neuron>
+}
+
+impl Layer {
+    pub fn new(num_inputs: u64, num_outputs: u64) -> Layer{
+        let mut neurons = Vec::with_capacity(num_outputs as usize);
+
+        for _ in 0..num_outputs {
+            let neuron = Neuron::new(num_inputs);
+            neurons.push(neuron);
+        }
+
+        Layer{neurons}
+    }
+
+    fn forward(&self, inputs: &Vec<f64>) -> Vec<f64> {
+        let mut outputs = Vec::with_capacity(self.neurons.len());
+
+        for neuron in &self.neurons{
+            let neuron_result = neuron.forward(&inputs);
+            outputs.push(neuron_result);
+        }
+
+        outputs
+    }
+}
+
+pub struct Network {
+   pub layers: Vec<Layer>
+}
+
+impl Network {
+    pub fn forward(&self, inputs: &Vec<f64>) -> Vec<f64> {
+        let mut result = Vec::new();
+
+        for (index, layer) in self.layers.iter().enumerate(){
+            if index == 0 {
+                // The first layer receives the inputs directly
+                result = layer.forward(&inputs);
+                continue
+            }
+
+            // Subsequent layers will receive the previous layers output
+            result = layer.forward(&result)
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{network};
+
+    #[test]
+    fn simple_network() {
+        let network = network::Network{
+            layers: vec![
+                network::Layer::new(3, 4),
+                network::Layer::new(4, 5),
+                network::Layer::new(5, 100),
+                network::Layer::new(100, 1),
+            ],
+        };
+
+        let inputs = vec![0.1, 0.2, 0.3];
+
+        let output = network.forward(&inputs);
+        
+        println!("{:?}", output);
+    }
+}
--- src/lib/value.rs
+++ src/lib/value.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Sub, Mul, Div, DerefMut, Deref};
+use std::ops::{Add, Sub, Mul, Div, Deref};
 use std::fmt;
 use std::rc::Rc;
 use std::cell::RefCell;
@@ -59,7 +59,9 @@ impl<T> Deref for Value<T> {
     }
 }
 
-impl<T: Copy> Value<T> {
+impl<T> Value<T>
+where T: Copy + Mul<f64, Output = f64> + Into<f64> + From<f64>
+{
     pub fn new(data: T) -> Value<T> {
         let inner_value = InnerValue {
             data,
@@ -71,19 +73,20 @@ impl<T: Copy> Value<T> {
         Value(Rc::new(RefCell::new(inner_value)))
     }
 
+    pub fn new_from_ref(data: &T) -> Value<T> {
+        Value::new(*data)
+    }
+
     pub fn backward(&self) {
         let val = self.borrow();
-
+        
         match val.symbol{
             "+" => {
                 let left_ancestor = &val.ancestors[0];
                 let right_ancestor = &val.ancestors[1];
-                
 
                 left_ancestor.borrow_mut().gradient += 1.0 * val.gradient;
                 right_ancestor.borrow_mut().gradient += 1.0 * val.gradient;
-                
-                println!("Addition backward pass!")
             },
             "-" => {
                 let left_ancestor = &val.ancestors[0];
@@ -91,8 +94,6 @@ impl<T: Copy> Value<T> {
 
                 left_ancestor.borrow_mut().gradient += 1.0 * val.gradient;
                 right_ancestor.borrow_mut().gradient += 1.0 * val.gradient;
-
-                println!("Subtraction backward pass!")
             }
             "*" => {
                 let left_ancestor = &val.ancestors[0];
@@ -100,27 +101,42 @@ impl<T: Copy> Value<T> {
 
                 left_ancestor.borrow_mut().gradient += right_ancestor.borrow().data * val.gradient;
                 right_ancestor.borrow_mut().gradient += left_ancestor.borrow().data * val.gradient;
+            }
+            "/" => {
+                let left_ancestor = &val.ancestors[0];
+                let right_ancestor = &val.ancestors[1];
 
-                println!("Subtraction backward pass!")
+                let right_ancestor_data: f64 = right_ancestor.borrow().data.into();
+                left_ancestor.borrow_mut().gradient += (1.0/right_ancestor_data) * val.gradient;
+                
+                let left_ancestor_data: f64 = left_ancestor.borrow().data.into();
+                right_ancestor.borrow_mut().gradient += (-left_ancestor_data/(right_ancestor_data * right_ancestor_data)) * val.gradient;
             }
             _ => ()
         }
     }
-    
+
     pub fn get_data(&self) -> T {
         self.borrow().data
     }
-    
+
     pub fn get_gradient(&self) -> f64 {
         self.borrow().gradient
     }
-    
+
     pub fn set_gradient(&self, gradient: f64) {
         self.borrow_mut().gradient = gradient;
     }
+    
+    pub fn clear_gradient(&self) {
+        self.borrow_mut().gradient = 0.0;
+        self.borrow_mut().ancestors.clear();
+    }
 }
 
-impl<T: Add<Output=T> + Copy + 'static> Add for Value<T> {
+impl<T> Add for Value<T>
+where T: Add<Output=T> + Copy + 'static + Mul<f64, Output = f64> + Into<f64> + From<f64>
+{
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -137,7 +153,9 @@ impl<T: Add<Output=T> + Copy + 'static> Add for Value<T> {
     }
 }
 
-impl<T: Add<Output=T> + Copy + 'static> Add for &Value<T> {
+impl<T> Add for &Value<T>
+where T: Add<Output=T> + Copy + 'static + Mul<f64, Output = f64> + Into<f64> + From<f64>
+{
     type Output = Value<T>;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -155,7 +173,9 @@ impl<T: Add<Output=T> + Copy + 'static> Add for &Value<T> {
 }
 
 
-impl<T: Sub<Output=T> + Copy + 'static> Sub for Value<T> {
+impl<T> Sub for Value<T>
+where T: Sub<Output=T> + Copy + 'static + Mul<f64, Output = f64> + Into<f64> + From<f64>
+{
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
@@ -173,7 +193,9 @@ impl<T: Sub<Output=T> + Copy + 'static> Sub for Value<T> {
 }
 
 
-impl<T: Sub<Output=T> + Copy + 'static> Sub for &Value<T> {
+impl<T> Sub for &Value<T>
+where T: Sub<Output=T> + Copy + 'static + Mul<f64, Output = f64> + Into<f64> + From<f64>
+{
     type Output = Value<T>;
 
     fn sub(self, rhs: Self) -> Self::Output {
@@ -190,71 +212,109 @@ impl<T: Sub<Output=T> + Copy + 'static> Sub for &Value<T> {
     }
 }
 
+impl<T> Mul for Value<T>
+where T: Mul<Output=T> + Copy + 'static + Mul<f64, Output = f64> + Into<f64> + From<f64>
+{
+    type Output = Self;
 
-//
-// impl<T: Mul<Output=T>> Mul for Value<T> {
-//     type Output = Self;
-//
-//     fn mul(self, rhs: Self) -> Self::Output {
-//         let result = self.data * rhs.data;
-//
-//         Value::new(result)
-//     }
-// }
-//
-// impl<T: Div<Output=T>> Div for Value<T> {
-//     type Output = Self;
-//
-//     fn div(self, rhs: Self) -> Self::Output {
-//         let result = self.data / rhs.data;
-//
-//         Value::new(result)
-//     }
-// }
-//
+    fn mul(self, rhs: Self) -> Self::Output {
+        let result =  self.borrow().data * rhs.borrow().data;
+        let value = Value::new(result);
 
-// Allows the value to be printed via {}
-// impl<T: fmt::Display> fmt::Display for Value<T>{
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         write!(f, "{}", self.data)
-//     }
-// }
-//
-// impl<T: fmt::Debug> fmt::Debug for Value<T>{
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         write!(f, "{}", self.data)
-//     }
-// }
+        let mut ancestors = vec![Rc::clone(&self), Rc::clone(&rhs)];
+
+        value.borrow_mut().ancestors.append(&mut ancestors);
+
+        value.borrow_mut().symbol = "*";
+
+        value
+    }
+}
+
+impl<T> Mul for &Value<T>
+where T: Mul<Output=T> + Copy + 'static + Mul<f64, Output = f64> + Into<f64> + From<f64>
+{
+    type Output = Value<T>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let result =  self.borrow().data * rhs.borrow().data;
+        let value = Value::new(result);
+
+        let mut ancestors = vec![Rc::clone(&self), Rc::clone(&rhs)];
+
+        value.borrow_mut().ancestors.append(&mut ancestors);
+
+        value.borrow_mut().symbol = "*";
+
+        value
+    }
+}
+
+impl<T> Div for Value<T>
+where T: Div<Output=T> + Copy + 'static + Mul<f64, Output = f64> + Into<f64> + From<f64>
+{
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        let result =  self.borrow().data / rhs.borrow().data;
+        let value = Value::new(result);
+
+        // Set a reference to the ancestors
+        let mut ancestors = vec![Rc::clone(&self), Rc::clone(&rhs)];
+        value.borrow_mut().ancestors.append(&mut ancestors);
+
+        value.borrow_mut().symbol = "/";
+
+        value
+    }
+}
+
+impl<T> Div for &Value<T>
+where T: Div<Output=T> + Copy + 'static + Mul<f64, Output = f64> + Into<f64> + From<f64>
+{
+    type Output = Value<T>;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        let result =  self.borrow().data / rhs.borrow().data;
+        let value = Value::new(result);
+
+        // Set a reference to the ancestors
+        let mut ancestors = vec![Rc::clone(&self), Rc::clone(&rhs)];
+        value.borrow_mut().ancestors.append(&mut ancestors);
+
+        value.borrow_mut().symbol = "/";
+
+        value
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::value;
-    use crate::value::{InnerValue, Value};
-    use std::rc::Rc;
+    use crate::value::{Value};
 
     #[test]
     fn simple_addition_on_values(){
         // Initial addition
-        let x = Value::new(1);
-        let w = Value::new(2);
+        let x = Value::new(1.0);
+        let w = Value::new(2.0);
 
         // Get a reference of x to assert on the gradient later
         let x_clone = x.clone();
 
         let y = x + w;
-        assert_eq!(y.get_data(), 3);
+        assert_eq!(y.get_data(), 3.0);
 
         // Get a reference to y to perform the backward pass
         let y_clone = y.clone();
 
         // Subsequent addition
-        let z = y + Value::new(10);
-        assert_eq!(z.get_data(), 13);
+        let z = y + Value::new(10.0);
+        assert_eq!(z.get_data(), 13.0);
 
         // Set the gradient for z and calculate the gradient
         // We start from z as it's the root node.
         z.set_gradient(1.0);
-        
+
         // Todo call in topological order
         z.backward();
         y_clone.backward();
@@ -265,22 +325,17 @@ mod tests {
 
     #[test]
     fn addition_references(){
-        let x = &Value::new(1);
-        let w = &Value::new(2);
+        let x = &Value::new(1.0);
+        let w = &Value::new(2.0);
 
         let y = x + w;
 
-        let z = &y + &Value::new(10);
+        let z = &y + &Value::new(10.0);
 
         z.set_gradient(1.0);
 
         z.backward();
         y.backward();
-
-        println!("⭐️ {:?}", z);
-        println!("⭐️ {:?}", y);
-
-        println!("dz/dx = {}", x.get_gradient());
 
         assert_eq!(x.get_gradient(), 1.0);
         assert_eq!(y.get_gradient(), 1.0);
@@ -288,25 +343,20 @@ mod tests {
 
     #[test]
     fn simple_subtraction_on_values(){
-        let x = Value::new(11);
-        let w = Value::new(2);
+        let x = Value::new(11.0);
+        let w = Value::new(2.0);
 
         let x_1 = x.clone();
 
         let y = x - w;
+
+        assert_eq!(y.get_data(), 9.0);
         
-        assert_eq!(y.get_data(), 9);
-
-        println!("⭐️ {:?}", y);
-
         y.set_gradient(1.0);
         y.backward();
-
-        println!("⭐️ {:?}", y);
-
+        
         assert_eq!(x_1.get_gradient(), 1.0);
     }
-
 
     #[test]
     fn subtraction_references(){
@@ -322,25 +372,79 @@ mod tests {
 
         z.backward();
         y.backward();
-
-        println!("⭐️ {:?}", z);
-        println!("⭐️ {:?}", y);
-
-        println!("dz/dx = {}", x.get_gradient());
-
+        
         assert_eq!(x.get_gradient(), 1.0);
         assert_eq!(y.get_gradient(), 1.0);
     }
 
-    // #[test]
-    // fn large_muls_on_values(){
-    //     // 2^32
-    //     let num = 2_f64.powi(32);
-    //
-    //     let a: value::Value<f64> = value::Value::new(num);
-    //
-    //     let c = a * a;
-    //
-    //     assert!(c.data > 0.0);
-    // }
+    #[test]
+    fn simple_multiplication_on_values(){
+        let x = Value::new(10.0);
+        let w = Value::new( 20.0);
+
+        let x_1 = x.clone();
+        let w_1 = w.clone();
+
+        let y = x * w;
+
+        y.set_gradient(1.0);
+        y.backward();
+
+        assert_eq!(x_1.get_gradient(), 20.0);
+        assert_eq!(w_1.get_gradient(), 10.0);
+    }
+
+    #[test]
+    fn multiplication_references(){
+        let x = &Value::new(10.0);
+        let w = &Value::new( 20.0);
+
+        let y = x * w;
+
+        assert_eq!(y.get_data(), 200.0);
+        assert_eq!(x.get_gradient(), 0.0);
+
+        y.set_gradient(1.0);
+        y.backward();
+        
+        assert_eq!(x.get_gradient(), 20.0);
+        assert_eq!(w.get_gradient(), 10.0);
+    }
+
+    #[test]
+    fn simple_div_on_values(){
+        let x = Value::new(100.0);
+        let w = Value::new( 2.0);
+
+        let x_1 = x.clone();
+        let w_1 = w.clone();
+
+        let y = x / w;
+
+        assert_eq!(y.get_data(), 50.0);
+        assert_eq!(x_1.get_gradient(), 0.0);
+
+        y.set_gradient(1.0);
+        y.backward();
+        
+        assert_eq!(x_1.get_gradient(), 0.5);
+        assert_eq!(w_1.get_gradient(), -25.0);
+    }
+
+    #[test]
+    fn div_on_references(){
+        let x = &Value::new(100.0);
+        let w = &Value::new( 2.0);
+
+        let y = x / w;
+
+        assert_eq!(y.get_data(), 50.0);
+        assert_eq!(x.get_gradient(), 0.0);
+
+        y.set_gradient(1.0);
+        y.backward();
+        
+        assert_eq!(x.get_gradient(), 0.5);
+        assert_eq!(w.get_gradient(), -25.0);
+    }
 }
